b889c128c45ee816e145382cfa3fad0ea9184321
BelCodeur <cruzjaydee15@gmail.com>
2025-08-04 12:34:00 +0100

    Ajout de auth/server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,27 +1,23 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import type { GetServerSidePropsContext } from 'next';
-import type { Database } from '@/types/database';
+// lib/supabase/server.ts
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import type { Database } from '@/types/database'
 
-export const createSupabaseServerClient = (context: GetServerSidePropsContext) => {
-  return createServerClient<Database>(
+export const createSupabaseServerClient  = () =>
+  createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name: string) => {
-          return context.req.cookies[name];
+        async get(name: string) {
+          return (await cookies()).get(name)?.value
         },
-        set: (name: string, value: string, options: CookieOptions) => {
-          context.res.setHeader('Set-Cookie', (context.res.getHeader('Set-Cookie') as string[] || []).concat(
-            `${name}=${value}; Path=${options.path}; Max-Age=${options.maxAge}; HttpOnly; Secure`
-          ));
+        async set(name: string, value: string, options) {
+          (await cookies()).set({ name, value, ...options })
         },
-        remove: (name: string, options: CookieOptions) => {
-          context.res.setHeader('Set-Cookie', (context.res.getHeader('Set-Cookie') as string[] || []).concat(
-            `${name}=; Path=${options.path}; Max-Age=0; HttpOnly; Secure`
-          ));
-        },
-      },
+        async remove(name: string, options) {
+          (await cookies()).set({ name, value: '', ...options })
+        }
+      }
     }
-  );
-};
+  )
